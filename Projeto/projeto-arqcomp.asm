d0adfa6005ba824aacf6a85a223f1667327346d6
Marcos <marcosolivee136@gmail.com>
2025-01-06 12:12:32 -0300

    Finalização da terra

--- Projeto/projeto-arqcomp.asm
+++ Projeto/projeto-arqcomp.asm
@@ -1,8 +1,13 @@
+.data
+	#test: 220 elementos, 0 ao 880
+dtCeu2:	.word 0x10012004, 0x10012008, 0x1001200c, 0x10012010, 0x10012014, 0x10012018, 0x1001201c, 0x10012020, 0x10012024, 0x10012028, 0x1001202c, 0x10012030, 0x10012034, 0x10012038, 0x1001203c, 0x10012040, 0x10012044, 0x10012048, 0x1001204c, 0x10012050, 0x10011e10, 0x10011e14, 0x10011e18, 0x10011e1c, 0x10011e20, 0x10011e24, 0x10011e28, 0x10011e2c, 0x10011e30, 0x10011e34, 0x10011e38, 0x10011e3c, 0x10011e40, 0x10011c1c, 0x10011c20, 0x10011c24, 0x10011c28, 0x10011c2c, 0x10011c30, 0x10011c34, 0x10012064, 0x10012068, 0x1001206c, 0x10012070, 0x10012074, 0x10012078, 0x1001207c, 0x10012080, 0x10012084, 0x10012088, 0x1001208c, 0x10012090, 0x10012094, 0x10012098, 0x1001209c, 0x100120a0, 0x100120a4, 0x100120a8, 0x100120ac, 0x100120b0, 0x10011e70, 0x10011e74, 0x10011e78, 0x10011e7c, 0x10011e80, 0x10011e84, 0x10011e88, 0x10011e8c, 0x10011e90, 0x10011e94, 0x10011e98, 0x10011e9c, 0x10011ea0, 0x10011c7c, 0x10011c80, 0x10011c84, 0x10011c88, 0x10011c8c, 0x10011c90, 0x10011c94, 0x100120c4, 0x100120c8, 0x100120cc, 0x100120d0, 0x100120d4, 0x100120d8, 0x100120dc, 0x100120e0, 0x100120e4, 0x100120e8, 0x100120ec, 0x100120f0, 0x100120f4, 0x100120f8, 0x100120fc, 0x10012100, 0x10012104, 0x10012108, 0x1001210c, 0x10012110, 0x10011ed0, 0x10011ed4, 0x10011ed8, 0x10011edc, 0x10011ee0, 0x10011ee4, 0x10011ee8, 0x10011eec, 0x10011ef0, 0x10011ef4, 0x10011ef8, 0x10011efc, 0x10011f00, 0x10011cdc, 0x10011ce0, 0x10011ce4, 0x10011ce8, 0x10011cec, 0x10011cf0, 0x10011cf4, 0x10012120, 0x10012124, 0x10012128, 0x1001212c, 0x10012130, 0x10012134, 0x10012138, 0x1001213c, 0x10012140, 0x10012144, 0x10012148, 0x1001214c, 0x10012150, 0x10012154, 0x10012158, 0x1001215c, 0x10012160, 0x10012164, 0x10012168, 0x10011f2c, 0x10011f30, 0x10011f34, 0x10011f38, 0x10011f3c, 0x10011f40, 0x10011f44, 0x10011f48, 0x10011f4c, 0x10011f50, 0x10011f54, 0x10011f58, 0x10011f5c, 0x10011d38, 0x10011d3c, 0x10011d40, 0x10011d44, 0x10011d48, 0x10011d4c, 0x10011d50, 0x1001217c, 0x10012180, 0x10012184, 0x10012188, 0x1001218c, 0x10012190, 0x10012194, 0x10012198, 0x1001219c, 0x100121a0, 0x100121a4, 0x100121a8, 0x100121ac, 0x100121b0, 0x100121b4, 0x100121b8, 0x100121bc, 0x10011f88, 0x10011f8c, 0x10011f90, 0x10011f94, 0x10011f98, 0x10011f9c, 0x10011fa0, 0x10011fa4, 0x10011fa8, 0x10011fac, 0x10011fb0, 0x10011fb4, 0x10011d98, 0x10011d9c, 0x10011da0, 0x10011da4, 0x10011da8, 0x100121c8, 0x100121cc, 0x100121d0, 0x100121d4, 0x100121d8, 0x100121dc, 0x100121e0, 0x100121e4, 0x100121e8, 0x100121ec, 0x100121f0, 0x100121f4, 0x100121f8, 0x100121fc, 0x10011fd4, 0x10011fd8, 0x10011fdc, 0x10011fe0, 0x10011fe4, 0x10011fe8, 0x10011fec, 0x10011ff0, 0x10011ff4, 0x10011ddc, 0x10011de0, 0x10011de4, 0x10011de8,
+detalheTerra:	.word 0x10016e00, 0x10016e04, 0x10016e0c, 0x10016e10, 0x10016e18, 0x10016e1c, 0x10016e24, 0x10016e28, 0x10016e30, 0x10016e34, 0x10016e3c, 0x10016e40, 0x10016e48, 0x10016e4c, 0x10016e54, 0x10016e58, 0x10016e60, 0x10016e64, 0x10016e6c, 0x10016e70, 0x10016e78, 0x10016e7c, 0x10016e84, 0x10016e88, 0x10016e90, 0x10016e94, 0x10016e9c, 0x10016ea0, 0x10016ea8, 0x10016eac, 0x10016eb4, 0x10016eb8, 0x10016ec0, 0x10016ec4, 0x10016ecc, 0x10016ed0, 0x10016ed8, 0x10016edc, 0x10016ee4, 0x10016ee8, 0x10016ef0, 0x10016ef4, 0x10016efc, 0x10016f00, 0x10016f08, 0x10016f0c, 0x10016f14, 0x10016f18, 0x10016f20, 0x10016f24, 0x10016f2c, 0x10016f30, 0x10016f38, 0x10016f3c, 0x10016f44, 0x10016f48, 0x10016f50, 0x10016f54, 0x10016f5c, 0x10016f60, 0x10016f68, 0x10016f6c, 0x10016f74, 0x10016f78, 0x10016f80, 0x10016f84, 0x10016f8c, 0x10016f90, 0x10016f98, 0x10016f9c, 0x10016fa4, 0x10016fa8, 0x10016fb0, 0x10016fb4, 0x10016fbc, 0x10016fc0, 0x10016fc8, 0x10016fcc, 0x10016fd4, 0x10016fd8, 0x10016fe0, 0x10016fe4, 0x10016fec, 0x10016ff0, 0x10016ff8, 0x10016ffc,
 .text
+
 memMngr:
 	lui $8, 0x1001
 	lui $16, 0x1004
-	addi $17, $0, 45
+	addi $17, $0, 306
 
 forMm:	beq $17, $0, fMm
 
@@ -15,18 +20,23 @@ forMm:	beq $17, $0, fMm
 	
 	j forMm
 	
-fMm:
-
+fMm:	
 
 main:	lui $8, 0x1001
+	lui $16, 0x1004
 	
 	
-	jal prmF1 #Céu
+	jal prmF1 #1º camada do céu
 	jal prmDetCeu1 #1º Detalhe do Céu
-	jal prmF2 #Primeira linha de grama
+	jal prmDetCeu2 #2º Detalhe do Céu
+	jal prmCeu2 #2º camada do céu
+	jal prmGramaTop1 #1º linha de grama
 	jal prmF4 #1º detalhe da grama
 	jal prmF5 #2º detalhe da grama
-	jal prmF3 #Terra
+	jal prmTerraBg #Terra Background
+	jal prmDetalheTerra1 #1º detalhe da terra
+	jal prmDetalheTerra2 #2º detalhe da terra
+	jal prmDetalheTerra3
 	jal finder
 	jal printVct
 	
@@ -91,7 +101,7 @@ cima: addi $8, $8, -512
       sw $9, +512($8)
       j cont 
       
-mark: ori $4, $0, 0x9FD5EF 
+mark: ori $4, $0, 0x5A4231
       sw $4, 0($8)
       sw $4, 32768($8)
       
@@ -119,7 +129,7 @@ printVct:
 forPVct:	
 	beq $17, $0, fPVct
 	
-	lw $4, 0($16)
+	lw $4, 880($16)
 	addi $2, $0, 34
 	syscall
 	
@@ -140,7 +150,7 @@ fPVct: jr $31
 
 #============printVct===========#
 	
-#===========CÉU==============#
+#===========CÉU-1==============#
 
 prmF1:	addi $9, $0, 7168
 	ori $4, $0, 0x8ECCEE
@@ -160,7 +170,30 @@ for1:	beq $8, $10, fimF1
 fimF1:	add $10, $0, $0
 	jr $31
 
-#===========CÉU==============#
+#===========CÉU-1==============#
+
+#===========CÉU-2==============#
+prmCeu2:
+	add $4, $0, $0
+	add $10, $0, $0
+	
+	ori $10, $0, 0x10012600
+	ori $11, $0, 0x10017000
+	ori $4, $0, 0xC8EEFF
+	
+forCeu2:
+	beq $10, $11, fimCeu2
+	
+	sw $4, 0($10)
+	
+	addi $10, $10, +4
+	
+	j forCeu2
+	
+fimCeu2:
+	jr $31
+
+#===========CÉU-2==============#
 
 #===========DETALHE-CÉU-1==============#
 
@@ -190,6 +223,31 @@ fimDetCeu1:
 
 #===========DETALHE-CÉU-1==============#
 
+#===========DETALHE-CÉU-2==============#
+
+prmDetCeu2:
+	lui $16, 0x1004
+	ori $4, 0x9FD5EF
+	add $9, $0, $0
+	
+forDetCeu2:
+	beq $9, 220, fimDetCeu2
+	
+	lw $5, 0($16)
+	sw $4, 0($5)
+	
+	addi $9, $9, +1
+	addi $16, $16, +4
+	
+	j forDetCeu2
+	
+	
+fimDetCeu2:
+	add $9, $0, $0
+	jr $31
+
+#===========DETALHE-CÉU-2==============#
+
 
 #=========ESTILINGUE===========#
 
@@ -205,31 +263,35 @@ fimDetCeu1:
 
 #=========ESTILINGUE===========#
 
-#===========GRAMA============#
+#===========GRAMA-TOP-1============#
 
-prmF2:	addi $11, $0, 128
+prmGramaTop1:	
+	ori $10, $0, 0x10016a00
+	ori $11, $0, 0x10016c00
 	ori $4, $0, 0x00A210
 	
-for2:	beq $11, $0, fimF2
+forGramaTop1:	
+	beq $10, $11, fimGramaTop1
 
-	sw $4, 0($8)
-	sw $4, 32768($8)
-	add $8, $8, 4
+	sw $4, 0($10)
+	sw $4, 32768($10)
+	add $10, $10, +4
 	
-	addi $11, $11, -1
-	
-	j for2
+	j forGramaTop1
 	
 
-fimF2: jr $31
+fimGramaTop1: 
+	jr $31
 
-#===========GRAMA============#
+#===========GRAMA-TOP-1============#
 
 #=======DETALHE-GRAMA-1========#
 
 prmF4:	addi $12, $0, 128
 	ori $13, $0, 0x00A210
-	ori $14, $0, 0x8B4513
+	ori $14, $0, 0x35261D
+	ori $8, $0, 0x10016c00
+	
 	
 for4:	beq $12, $0, fimF4
 	
@@ -271,7 +333,8 @@ FTerra:add $15, $0, $0
 
 prmF5:	addi $12, $0, 128
 	ori $13, $0, 0x00A210
-	ori $14, $0, 0x8B4513
+	ori $14, $0, 0x35261D
+	ori $8, $0, 0x10016e00
 	
 for5:	beq $12, $0, fimF5
 	
@@ -308,30 +371,85 @@ FGrama:add $15, $0, $0
 	
 #=======DETALHE-GRAMA-2========#
 
+#===========TERRA-BG============#
 
+prmTerraBg:	
+	ori $10, $0, 0x10017000
+	ori $11, $0, 0x10018000
+	ori $4, $0, 0x35261D
+	
+forTerraBg:	
+	beq $10, $11, fimTerraBg
 
-#===========TERRA============#
+	sw $4, 0($10)
+	sw $4, 32768($10)
+	
+	add $10, $10, +4
+	
+	j forTerraBg
 
-prmF3:	addi $10, $0, 640
-	ori $4, $0, 0x8B4513
+fimTerraBg:	
+	jr $31
 	
-for3:	beq $10, $0, fimF3
+#===========TERRA-BG============#	
 
-	sw $4, 0($8)
-	sw $4, 32768($8)
+#===========DETALHE-TERRA-1============#	
+prmDetalheTerra1:
+	ori $10, $0, 0x10017400
+	ori $11, $0, 0x10017800
+	ori $4, $0, 0x5A4231
 	
-	add $8, $8, 4
+forDetalheTerra1:
+	beq $10, $11, fimDetalheTerra1
 	
-	addi $10, $10, -1
+	sw $4, 0($10)
 	
-	j for3
+	addi $10, $10, +4
+	
+	j forDetalheTerra1
 
-fimF3:	jr $31
+fimDetalheTerra1:
+	jr $31
+#===========DETALHE-TERRA-1============#
 	
-#===========TERRA============#	
+#===========DETALHE-TERRA-2============#	
+prmDetalheTerra2:
+	ori $10, $0, 0x10017c00
+	ori $11, $0, 0x10017e00
+	ori $4, $0, 0x5A4231
 	
+forDetalheTerra2:
+	beq $10, $11, fimDetalheTerra2
+	
+	sw $4, 0($10)
+	
+	addi $10, $10, +4
+	
+	j forDetalheTerra2
 
-	
+fimDetalheTerra2:
+	jr $31
+#===========DETALHE-TERRA-2============#	
 
+#===========DETALHE-TERRA-3============#	
+prmDetalheTerra3:
+	lui $16, 0x1004
+	ori $4, $0, 0x5A4231
+	add $9, $0, $0
+	
+forDetalheTerra3:
+	beq $9, 86, fimDetalheTerra3
+	
+	lw $10, 880($16)
+	sw $4, 0($10)
+	
+	addi $16, $16, +4
+	addi $9, $9, +1
+	
+	j forDetalheTerra3
+
+fimDetalheTerra3:
+	jr $31
+	
 	
 	
